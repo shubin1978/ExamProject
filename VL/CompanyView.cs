3f4d8d9dff020c6f7223cd1519c1a0caccc6ce4c
yury <shubin1978@omnicommdv.ru>
2023-11-04 17:08:27 +0800

    Добавление уровня взаимодействия с пользователем

---
+++ VL/CompanyView.cs
@@ -0,0 +1,33 @@
+﻿using Object = ProjectApp.Model.Object;
+
+namespace ProjectApp.CLI;
+
+public static class CompanyView
+{
+    public static void ShowObject(Object obj)
+    {
+        CLI.PrintInfo($"{obj.At} {obj.Title} : {obj.Position} -> {obj.Status}");
+        foreach (var u in obj.Equipment)
+        {
+            CLI.PrintInfo($"{u.Cathegory }:{ u.Id}");
+        }
+    }
+
+    public static void ShowObjects(List<Object> objects)
+    {
+        foreach (var o in objects)
+        {
+            ShowObject(o);
+        }
+    }
+
+    public static void PrintMenu()
+    {
+        CLI.PrintInfo("= M E N U = ");
+        CLI.PrintInfo("1- Список всей техники");
+        CLI.PrintInfo("2- Поиск объектов по локации");
+        CLI.PrintInfo("3- Поиск объектов по номеру ");
+        CLI.PrintInfo("4- Добавить объект");
+        CLI.PrintInfo("0- Завершение работы ");
+    }
+} 
