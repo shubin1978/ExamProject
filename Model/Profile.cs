d3f5bd9573a08e2cc263726eecf08ea8bca50ff7
yury <shubin1978@omnicommdv.ru>
2023-11-04 16:07:24 +0800

    Добавление Profile

---
+++ Model/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ProjectApp.Model;
+
+public record Profile
+{
+    public int At { get; set; }
+    public string Title { get; set; }
+    public string Position { get; set; }
+    public Status Status { get; set; }
+   
+[JsonIgnore]
+    public string FullInfo => $"{At}, {Title} --> [{Position}] : {Status} ";
+}
